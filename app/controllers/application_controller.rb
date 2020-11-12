5f16d3c802462eaa9e13a820f9471d59a3071602
Griefcat <anardolilli@gmail.com>
2020-11-12 12:19:44 -0500

    login adjusted, navbar adjusted, Your Courses adjusted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,11 @@ class ApplicationController < ActionController::Base
         end 
     end 
 
+    def current_user
+        @current_user = User.find(session[:user_id])
+      end
+
+helper_method :current_user
 helper_method :logged_in?
 
 end
--- app/models/users_course.rb
+++ app/models/users_course.rb
@@ -1,4 +1,5 @@
 class UsersCourse < ApplicationRecord
     belongs_to :user
     belongs_to :course
+    validates :course, uniqueness: :true
 end
